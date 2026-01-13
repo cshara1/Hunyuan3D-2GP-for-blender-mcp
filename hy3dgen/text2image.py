acb5a91feae69493b258d6a7eedb1741bc126d8d
cshara1 <gauuud+cshara1@gmail.com>
2026-01-13 15:47:50 +0900

    text2image device

--- hy3dgen/text2image.py
+++ hy3dgen/text2image.py
@@ -40,7 +40,8 @@ class HunyuanDiTPipeline:
             torch_dtype=torch.float16,
             enable_pag=True,
             pag_applied_layers=["blocks.(16|17|18|19)"]
-        ) # .to(device) #  needed to avoid displaying the warning
+        ).to(self.device)
+        
         self.pos_txt = ",白色背景,3D风格,最佳质量"
         self.neg_txt = "文本,特写,裁剪,出框,最差质量,低质量,JPEG伪影,PGLY,重复,病态," \
                        "残缺,多余的手指,变异的手,画得不好的手,画得不好的脸,变异,畸形,模糊,脱水,糟糕的解剖学," \
@@ -67,7 +68,7 @@ class HunyuanDiTPipeline:
     @torch.no_grad()
     def __call__(self, prompt, seed=0):
         seed_everything(seed)
-        generator = torch.Generator(device="cuda") #self.pipe.device
+        generator = torch.Generator(device=self.device) 
         generator = generator.manual_seed(int(seed))
         out_img = self.pipe(
             prompt=prompt[:60] + self.pos_txt,
